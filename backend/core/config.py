c070c875ce6e749dea8e819ae3d1599df44b846a
flavien-hugs <geekactux@gmail.com>
2022-12-05 01:38:00 +0000

    Intalled package for Database managment and connecting projet to a database

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,6 +1,21 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
+
+
 class Settings:
-    PROJECT_NAME: str = "Job Board API"
     PROJECT_VERSION: str = "1.0.0"
+    PROJECT_NAME: str = "Job Board API"
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
+    POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
 
 settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,9 @@ from fastapi.staticfiles import StaticFiles
 from .core.config import settings
 from .api.page.home import page_router
 
+from .db.session import engine
+from .db.base_class import Base
+
 
 def include_router(app):
     app.include_router(page_router)
@@ -12,10 +15,16 @@ def include_router(app):
 def configure_static(app):
     app.mount("/static", StaticFiles(directory="backend/static"), name="static")
 
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
     configure_static(app)
+    create_tables()
     return app
 
 
