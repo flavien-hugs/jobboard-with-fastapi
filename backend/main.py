3525536bcc34191a340fc7388dee73a34a885cff
flavien-hugs <geekactux@gmail.com>
2022-12-05 01:15:16 +0000

    Install package to serving static files

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .core.config import settings
 from .api.page.home import page_router
@@ -8,9 +9,13 @@ def include_router(app):
     app.include_router(page_router)
 
 
+def configure_static(app):
+    app.mount("/static", StaticFiles(directory="backend/static"), name="static")
+
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     include_router(app)
+    configure_static(app)
     return app
 
 
