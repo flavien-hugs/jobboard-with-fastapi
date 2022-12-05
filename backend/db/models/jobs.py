0f6a1d8d300c45d4e06f0f1189aaed4d9e648a45
flavien-hugs <geekactux@gmail.com>
2022-12-05 03:00:34 +0000

    Add schemas fo users

---
+++ backend/db/models/jobs.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Boolean,Date, ForeignKey
+from sqlalchemy.orm import relationship
+
+from ..base_class import Base
+
+
+class Job(Base):
+    job_id = Column(Integer, primary_key=True, index=True)
+    job_title = Column(String, nullable=False)
+    job_company = Column(String, nullable=False)
+    job_company_url = Column(String)
+    job_location = Column(String, nullable=False)
+    job_description = Column(String, nullable=False)
+    job_date_posted = Column(Date)
+    job_is_active = Column(Boolean(), default=True)
+    job_owner_id = Column(Integer, ForeignKey("user.id"))
+    job_owner = relationship("User", back_populates="jobs")
