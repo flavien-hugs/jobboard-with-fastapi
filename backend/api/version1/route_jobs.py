0f5b65ec3a0ad66d2831483a51d570958f2c0c96
flavien-hugs <geekactux@gmail.com>
2022-12-08 19:21:45 +0000

    Change create job and user route

--- backend/api/version1/route_jobs.py
+++ backend/api/version1/route_jobs.py
@@ -9,7 +9,7 @@ from backend.db.repository.jobs import create_new_job
 job_router = APIRouter()
 
 
-@job_router.post("/create_job/", response_model=ShowJob)
+@job_router.post("/create/", response_model=ShowJob)
 def create_job(job: JobCreate, db: Session = Depends(get_db)):
     current_user = 1
     job = create_new_job(job=job, db=db, job_owner_id=current_user)
--- backend/api/version1/route_users.py
+++ backend/api/version1/route_users.py
@@ -8,7 +8,7 @@ from backend.db.repository.users import create_new_user
 users_router = APIRouter()
 
 
-@users_router.post("/", response_model=ShowUser)
+@users_router.post("/register/", response_model=ShowUser)
 def create_user(user: UserCreate, db: Session = Depends(get_db)):
     user = create_new_user(user=user, db=db)
     return user
