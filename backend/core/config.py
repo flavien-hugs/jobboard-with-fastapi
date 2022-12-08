ac04ddab74488b7813bf5f4dfed2a28d12632c4f
flavien-hugs <geekactux@gmail.com>
2022-12-08 21:22:48 +0000

    Install packages for jwt and create jwt token

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 from pathlib import Path
 
 from dotenv import load_dotenv
@@ -16,6 +17,10 @@ class Settings:
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES = (timedelta(days=7),)
 
 
 settings = Settings()
