2bce901f3fd62a38db6da391acec7b545c3d8eed
flavien-hugs <geekactux@gmail.com>
2022-12-11 12:57:16 +0000

    add authentication and permissions

--- backend/api/version1/route_login.py
+++ backend/api/version1/route_login.py
@@ -4,7 +4,10 @@ from fastapi import APIRouter
 from fastapi import Depends
 from fastapi import HTTPException
 from fastapi import status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.security import OAuth2PasswordRequestForm
+from jose import jwt
+from jose import JWTError
 from sqlalchemy.orm import Session
 
 from backend.core.config import settings
@@ -27,7 +30,7 @@ def authenticate_user(email: str, password: str, db: Session):
     return user
 
 
-@login_router.post("/", response_model=Token)
+@login_router.post("/token/", response_model=Token)
 def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
 ):
@@ -42,3 +45,29 @@ def login_for_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token")
+
+
+def get_current_user_from_token(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        email: str = payload.get("sub")
+        print("email/email extracted is ", email)
+        if email is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+    user = get_user(email=email, db=db)
+    if user is None:
+        raise credentials_exception
+    return user
