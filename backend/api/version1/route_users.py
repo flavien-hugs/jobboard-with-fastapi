55715267df6aab01bfabce54a734cc7407caed30
flavien-hugs <geekactux@gmail.com>
2022-12-11 14:03:40 +0000

    add routes get users all

--- backend/api/version1/route_users.py
+++ backend/api/version1/route_users.py
@@ -1,13 +1,22 @@
+from typing import List
+
 from fastapi import APIRouter
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from backend.db.repository.users import create_new_user
+from backend.db.repository.users import list_users
 from backend.db.session import get_db
 from backend.schemas.users import ShowUser
 from backend.schemas.users import UserCreate
 
 users_router = APIRouter()
+
+
+@users_router.get("/", response_model=List[ShowUser])
+def read_users(db: Session = Depends(get_db)):
+    users = list_users(db=db)
+    return users
 
 
 @users_router.post("/register/", response_model=ShowUser)
