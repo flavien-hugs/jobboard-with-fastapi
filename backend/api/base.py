fdd2ceec257d8f3fcc0271232a35be8c748219c2
flavien-hugs <geekactux@gmail.com>
2022-12-11 14:45:16 +0000

    add parameters to api router

--- backend/api/base.py
+++ backend/api/base.py
@@ -5,7 +5,7 @@ from .version1 import route_login
 from .version1 import route_pages
 from .version1 import route_users
 
-api_router = APIRouter()
+api_router = APIRouter(include_in_schema=False)
 
 api_router.include_router(route_pages.pages_router, prefix="", tags=["pages"])
 api_router.include_router(route_users.users_router, prefix="/users", tags=["users"])
