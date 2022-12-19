1c8b21287ce9289c7dc38472f8746a77e2e5e1f2
flavien-hugs <geekactux@gmail.com>
2022-12-19 10:08:19 +0000

    Update base route

--- backend/api/base.py
+++ backend/api/base.py
@@ -2,12 +2,10 @@ from fastapi import APIRouter
 
 from .version1 import route_jobs
 from .version1 import route_login
-from .version1 import route_pages
 from .version1 import route_users
 
-api_router = APIRouter(include_in_schema=False)
+api_router = APIRouter()
 
-api_router.include_router(route_pages.pages_router, prefix="", tags=["pages"])
 api_router.include_router(route_users.users_router, prefix="/users", tags=["users"])
 api_router.include_router(route_jobs.job_router, prefix="/jobs", tags=["jobs"])
 api_router.include_router(route_login.login_router, prefix="/login", tags=["login"])
